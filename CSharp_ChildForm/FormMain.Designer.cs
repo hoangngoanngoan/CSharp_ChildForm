fcadb2e81c69c96cdc860db2a5647385722987d8
hoangngoanngoan <ldh2381995@gmail.com>
2024-08-12 19:11:00 +0700

    Add project files.

---
+++ CSharp_ChildForm/FormMain.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace CSharp_ChildForm
+{
+    partial class FormMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            formToolStripMenuItem = new ToolStripMenuItem();
+            formToolStripMenuItem1 = new ToolStripMenuItem();
+            form1ToolStripMenuItem = new ToolStripMenuItem();
+            form2ToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { formToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // formToolStripMenuItem
+            // 
+            formToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { formToolStripMenuItem1 });
+            formToolStripMenuItem.Name = "formToolStripMenuItem";
+            formToolStripMenuItem.Size = new Size(48, 20);
+            formToolStripMenuItem.Text = "Open";
+            // 
+            // formToolStripMenuItem1
+            // 
+            formToolStripMenuItem1.DropDownItems.AddRange(new ToolStripItem[] { form1ToolStripMenuItem, form2ToolStripMenuItem });
+            formToolStripMenuItem1.Name = "formToolStripMenuItem1";
+            formToolStripMenuItem1.Size = new Size(180, 22);
+            formToolStripMenuItem1.Text = "Form";
+            // 
+            // form1ToolStripMenuItem
+            // 
+            form1ToolStripMenuItem.Name = "form1ToolStripMenuItem";
+            form1ToolStripMenuItem.Size = new Size(180, 22);
+            form1ToolStripMenuItem.Text = "Form1";
+            form1ToolStripMenuItem.Click += form1ToolStripMenuItem_Click;
+            // 
+            // form2ToolStripMenuItem
+            // 
+            form2ToolStripMenuItem.Name = "form2ToolStripMenuItem";
+            form2ToolStripMenuItem.Size = new Size(180, 22);
+            form2ToolStripMenuItem.Text = "Form2";
+            form2ToolStripMenuItem.Click += form2ToolStripMenuItem_Click;
+            // 
+            // FormMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            IsMdiContainer = true;
+            MainMenuStrip = menuStrip1;
+            Name = "FormMain";
+            Text = "FormMain";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem formToolStripMenuItem;
+        private ToolStripMenuItem formToolStripMenuItem1;
+        private ToolStripMenuItem form1ToolStripMenuItem;
+        private ToolStripMenuItem form2ToolStripMenuItem;
+    }
+}
---
+++ CSharp_ChildForm/FormMain.cs
@@ -0,0 +1,24 @@
+namespace CSharp_ChildForm
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void form1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1 f1 = new Form1();
+            f1.MdiParent = this;
+            f1.Show();
+        }
+
+        private void form2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.MdiParent = this;
+            f2.Show();
+        }
+    }
+}
